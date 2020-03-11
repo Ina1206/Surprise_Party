1f5d4056e160ffc965b1ddaf613e32af1c644a39
hera <hera@PC-09>
2020-03-11 18:53:48 +0900

    床の表示設定

--- Surprise_Party/Surprise_Party/SourceCode/Scene/GameMain/GhostSpeakStage/CGhostSpeakStage.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/GameMain/GhostSpeakStage/CGhostSpeakStage.cpp
@@ -1,10 +1,14 @@
 #include "CGhostSpeakStage.h"
 
 CGhostSpeakStage::CGhostSpeakStage()
+	: CGhostSpeakStage	(0)
 {
 }
 
-CGhostSpeakStage::CGhostSpeakStage(int stageNum)
+CGhostSpeakStage::CGhostSpeakStage(int stageNum)	
+	: m_pCFontResource	(nullptr)
+	, changestr			()
+	, m_pCFloor			(nullptr)
 {
 	m_StageNum = stageNum;
 	//初期化処理関数.
@@ -30,10 +34,6 @@ void CGhostSpeakStage::UpDate(const bool& ControlFlag)
 
 	static int num = 0;
 	if (GetAsyncKeyState(VK_F3) & 0x0001) {
-		//削除処理関数.
-		if (m_pCFontResource->GetFont() != nullptr) {
-			m_pCFontResource->Destroy();
-		}
 		//読み込み処理関数.
 		m_pCFontResource->Load(changestr[num]);
 		num++;
@@ -45,6 +45,24 @@ void CGhostSpeakStage::UpDate(const bool& ControlFlag)
 		}
 	}
 
+	if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+		m_stLight.m_fLightWidth += 0.1f;
+	}
+	if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
+		m_stLight.m_fLightWidth -= 0.1f;
+	}
+	if (GetAsyncKeyState(VK_UP) & 0x8000) {
+		m_stLight.fIntensity += 0.1f;
+	}
+	if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+		m_stLight.fIntensity -= 0.1f;
+	}
+	if (GetAsyncKeyState('Z') & 0x8000) {
+		m_stLight.vPos.y += 0.1f;
+	}
+	if (GetAsyncKeyState('X') & 0x8000) {
+		m_stLight.vPos.y -= 0.1f;
+	}
 }
 
 //=========================================.
@@ -52,11 +70,20 @@ void CGhostSpeakStage::UpDate(const bool& ControlFlag)
 //=========================================.
 void CGhostSpeakStage::Render()
 {
+	//床の描画.
+	m_pCFloor->SetCameraPos(m_Camera.vPos);
+	m_pCFloor->RenderInitSetting(m_mView, m_mProj, m_stLight);
+	m_pCFloor->SetPos(D3DXVECTOR3(0.0f, -10.0f, 0.0f));
+	m_pCFloor->Render();
+	m_pCWall->SetCameraPos(m_Camera.vPos);
+	m_pCWall->RenderInitSetting(m_mView, m_mProj, m_stLight);
+	m_pCWall->SetPos(D3DXVECTOR3(0.0f, 0.0f, 0.0f));
+	m_pCWall->Render();
 
+	//文字の描画.
 	if (m_pCFontResource != nullptr) {
 		m_pCFontResource->String_Render();
 	}
-
 
 
 }
@@ -66,23 +93,28 @@ void CGhostSpeakStage::Render()
 //=========================================.
 void CGhostSpeakStage::Init()
 {
-	m_pCFontResource = CResourceManager::GetResourceManagerInstance()->GetFont();
-	//位置設定処理関数
-	m_pCFontResource->SetStartPos(D3DXVECTOR3(50.0f, 0.0f, 0.0f));
-	std::string str = "眠い";
-	//読み込み処理関数.
-	m_pCFontResource->Load(str);
 
-	//ファイル読み込み.
-	CFile* m_pCFile = new CFile();
-	m_pCFile->FileInput("Data\\File\\Test.csv");
+	//床.
+	m_pCFloor.reset(new CFloor());
+	m_pCFloor->SetScale(0.5f);
 
-	for (int splite = 0; splite < m_pCFile->GetColumnMax(); splite++) {
-		changestr.push_back(m_pCFile->GetLineData(splite));
-	}
-	m_pCFile->Close();
-	SAFE_DELETE(m_pCFile);
+	m_pCWall.reset(new CWall());
 
+	//文章読み込み処理関数.
+	LoadString();
+
+	m_stLight.fIntensity = 10.0f;
+	m_stLight.fLightPosWidth = 20.0f;
+	m_stLight.m_fLightWidth = 10.0f;
+	m_stLight.m_fLightMax = 1.0f;
+	m_stLight.vLightColor = D3DXVECTOR3(1.0f, 201.0f /255.0f, 14.0f / 255.0f);
+	m_stLight.vPos = D3DXVECTOR3(26.2f, 3.6f, 6.7f);
+	D3DXMatrixRotationYawPitchRoll(&m_stLight.mRot, 0.0f, 0.0f, 0.0f);
+	m_stLight.fIntensity = 119.0f;
+	m_stLight.m_fLightWidth = 6.2f;
+
+	m_Camera.vPos = D3DXVECTOR3(5.0f, 2.0f, -3.5f);
+	m_Camera.vLook = D3DXVECTOR3(5.0f, 0.0f, 2.0f);
 }
 
 //=========================================.
@@ -99,4 +131,28 @@ void CGhostSpeakStage::Release()
 void CGhostSpeakStage::Control()
 {
 
+}
+
+//=========================================.
+//		文章読み込み処理関数.
+//=========================================.
+void CGhostSpeakStage::LoadString()
+{
+	//ファイル読み込み.
+	CFile* m_pCFile = new CFile();
+	m_pCFile->FileInput("Data\\File\\Test.csv");
+
+	//ファイルの中の全文章設定.
+	for (int splite = 0; splite < m_pCFile->GetColumnMax(); splite++) {
+		changestr.push_back(m_pCFile->GetLineData(splite));
+	}
+	m_pCFile->Close();
+	SAFE_DELETE(m_pCFile);
+
+
+	m_pCFontResource = CResourceManager::GetResourceManagerInstance()->GetFont();
+	//読み込み処理関数.
+	m_pCFontResource->Load(changestr[0]);
+	//位置設定処理関数
+	m_pCFontResource->SetStartPos(D3DXVECTOR3(50.0f, 0.0f, 0.0f));
 }
--- Surprise_Party/Surprise_Party/SourceCode/Scene/GameMain/GhostSpeakStage/CGhostSpeakStage.h
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/GameMain/GhostSpeakStage/CGhostSpeakStage.h
@@ -3,6 +3,8 @@
 
 #include "..\StageBase\CStageBase.h"
 #include "..\..\..\FileLoad\FileResource\CFileResource.h"
+#include "..\..\GameObject\Object\StaticObject\Floor\CFloor.h"
+#include "..\..\GameObject\Object\StaticObject\Wall\CWall.h"
 
 /*****************************************
 *	お化けとの会話ステージクラス.
@@ -24,11 +26,13 @@ private:
 	//========================関数=============================//.
 	void Init();								//初期化処理関数.
 	void Release();								//解放処理関数.
+	void LoadString();							//文章読み込み処理関数.
 
 	//========================関数=============================//.
 	CFontResource*					m_pCFontResource;
 	std::vector<std::string>		changestr;
-
+	std::unique_ptr<CFloor>			m_pCFloor;			//床.
+	std::unique_ptr<CWall>			m_pCWall;			//床.
 
 };
 
