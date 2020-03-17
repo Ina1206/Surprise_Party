91bb2927633eef11a5feef70a10ad3196a823934
hera <hera@PC-09>
2020-03-17 11:45:48 +0900

    CSleepEffectクラスの追加

--- Surprise_Party/Surprise_Party/SourceCode/SpriteEffect/SleepEffect/CSleepEffect.h
+++ Surprise_Party/Surprise_Party/SourceCode/SpriteEffect/SleepEffect/CSleepEffect.h
@@ -1,2 +1,27 @@
-
+#ifndef CSLEEP_EFFECT_H
 #define CSLEEP_EFFECT_H
+
+#include "..\CSpriteEffectBase.h"
+
+/********************************************
+*		寝ているエフェクトクラス.
+****************/
+class CSleepEffect
+	: public CSpriteEffectBase
+{
+public:
+	CSleepEffect();
+	~CSleepEffect();
+
+	//===============関数=================//.
+	void Update();		//更新処理関数.
+
+private:
+	//===============関数=================//.
+	void Init();		//初期化処理関数.
+	void Release();		//解放処理関数.
+
+
+};
+
+#endif	//#ifndef CSLEEP_EFFECT_H.
