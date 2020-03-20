61325e534eb435802b5c11f37029a9fc0b68bdd2
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-03-21 01:36:50 +0900

    カメラの位置変更

--- Surprise_Party/Surprise_Party/SourceCode/Scene/GameMain/GhostSpeakStage/CGhostSpeakStage.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/GameMain/GhostSpeakStage/CGhostSpeakStage.cpp
@@ -131,6 +131,9 @@ void CGhostSpeakStage::Init()
 
 	m_Camera.vPos = D3DXVECTOR3(5.0f, 2.5f, -3.5f);
 	m_Camera.vLook = D3DXVECTOR3(5.0f, 2.5f, 5.0f);
+
+	m_Camera.vPos = D3DXVECTOR3(6.0f, 2.5f, 0.0f);
+	m_Camera.vLook = D3DXVECTOR3(6.0f, 3.0f, 5.0f);
 }
 
 //=========================================.
