633c8d3c505d130aab4bf9480157968cef5cf70f
hera <hera@PC-09>
2020-03-26 16:04:52 +0900

    コメント追加

--- Surprise_Party/Surprise_Party/SourceCode/Scene/Pause/CPause.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/Pause/CPause.cpp
@@ -28,6 +28,18 @@ CPause::~CPause()
 //========================================.
 void CPause::UpDate()
 {
+	//決定後の処理.
+	if (m_ChangeCnt > 0) {
+		m_ChangeCnt++;
+
+		if (m_ChangeCnt >= CHANGE_CNT_MAX) {
+			m_bPauseFlag = false;
+			m_vUV[CURSOR_NUM] = NORMAL_UV_POS;
+			m_ChangeCnt = 0;
+		}
+		return;
+	}
+
 	if (GetAsyncKeyState(VK_UP) & 0x0001) {
 		m_SelectNum--;
 		if (m_SelectNum < 0) {
@@ -49,16 +61,6 @@ void CPause::UpDate()
 		m_ChangeCnt++;
 	}
 
-	if (m_ChangeCnt > 0) {
-		m_ChangeCnt++;
-
-		if (m_ChangeCnt >= CHANGE_CNT_MAX) {
-			m_bPauseFlag = false;
-			m_vUV[CURSOR_NUM] = NORMAL_UV_POS;
-			m_ChangeCnt = 0;
-		}
-		return;
-	}
 
 	m_vPos[CURSOR_NUM].y = CURSOR_POS.y + (SELECT_STRING_WIDHT * m_SelectNum);
 }
@@ -69,9 +71,13 @@ void CPause::UpDate()
 void CPause::Render()
 {
 	for (unsigned int sprite = 0; sprite < m_pCSpriteUI.size(); sprite++) {
+		//UV座標設定.
 		m_pCSpriteUI[sprite]->SetPattern(m_vUV[sprite]);
+		//角度.
 		m_pCSpriteUI[sprite]->SetRotation(m_vRot[sprite]);
+		//座標.
 		m_pCSpriteUI[sprite]->SetPosition(m_vPos[sprite]);
+		//描画.
 		m_pCDepthStenceil->SetDepth(false);
 		m_pCSpriteUI[sprite]->Render();
 		m_pCDepthStenceil->SetDepth(true);
@@ -86,18 +92,24 @@ void CPause::Init()
 	m_pCResourceManager = CResourceManager::GetResourceManagerInstance();
 	m_pCDepthStenceil	= CDepth_Stencil::GetDepthStencilInstance();
 
+	//要素数設定.
 	m_pCSpriteUI.resize(UI_MAX);
 	m_vPos.reset(new D3DXVECTOR3[m_pCSpriteUI.size()]);
 	m_vRot.reset(new D3DXVECTOR3[m_pCSpriteUI.size()]);
 	m_vUV.reset(new D3DXVECTOR2[m_pCSpriteUI.size()]);
 
+	//スプライト設定.
 	for (int sprite = 0; sprite < SELECT_STRING_MAX; sprite++) {
+		//選択文章.
 		int SpriteNum = static_cast<int>(enSpriteUI::Continue) + sprite;
 		m_pCSpriteUI[sprite] = m_pCResourceManager->GetSpriteUI(static_cast<enSpriteUI>(SpriteNum));
 	}
+	//カーソル.
 	m_pCSpriteUI[CURSOR_NUM] = m_pCResourceManager->GetSpriteUI(enSpriteUI::GhostCursor);
+	//ポーズ.
 	m_pCSpriteUI[PAUSE_NUM] = m_pCResourceManager->GetSpriteUI(enSpriteUI::Pause);
 
+	//初期化.
 	for (unsigned int sprite = 0; sprite < m_pCSpriteUI.size(); sprite++) {
 		m_vUV[sprite] = INIT_UV;
 		m_vRot[sprite] = INIT_ROT;
@@ -110,7 +122,6 @@ void CPause::Init()
 		m_vPos[sprite] = INIT_POS;
 	}
 	m_vPos[CURSOR_NUM] = CURSOR_POS;
-
 	m_vRot[CURSOR_NUM] = CURSOR_ROT;
 }
 
--- Surprise_Party/Surprise_Party/SourceCode/Scene/Pause/CPause.h
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/Pause/CPause.h
@@ -22,14 +22,14 @@ public:
 	const float	SELECT_STRING_WIDHT = 100.0f;	//選択文章幅.
 	const float CHANGE_CNT_MAX		= 10;		//変更カウント最大数.
 
-	const D3DXVECTOR3	SELECT_STRING_POS	= D3DXVECTOR3(700.0f, 400.0f, 0.0f);
-	const D3DXVECTOR3	CURSOR_POS			= D3DXVECTOR3(800.0f, 450.0f, 0.0f);
-	const D3DXVECTOR3	INIT_POS			= D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-	const D3DXVECTOR3	INIT_ROT			= D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-	const D3DXVECTOR3	CURSOR_ROT			= D3DXVECTOR3(0.0f, 3.0f, 0.0f);
-	const D3DXVECTOR2	INIT_UV				= D3DXVECTOR2(0.0f, 0.0f);
-	const D3DXVECTOR2	NORMAL_UV_POS		= D3DXVECTOR2(0.0f, 0.0f);	
-	const D3DXVECTOR2	ENTER_UV_POS		= D3DXVECTOR2(0.0f, 1.0f);
+	const D3DXVECTOR3	SELECT_STRING_POS	= D3DXVECTOR3(700.0f, 400.0f, 0.0f);	//選択文章座標.
+	const D3DXVECTOR3	CURSOR_POS			= D3DXVECTOR3(800.0f, 450.0f, 0.0f);	//カーソル座標.
+	const D3DXVECTOR3	INIT_POS			= D3DXVECTOR3(0.0f, 0.0f, 0.0f);		//初期座標.
+	const D3DXVECTOR3	INIT_ROT			= D3DXVECTOR3(0.0f, 0.0f, 0.0f);		//初期角度.
+	const D3DXVECTOR3	CURSOR_ROT			= D3DXVECTOR3(0.0f, 3.0f, 0.0f);		//カーソル角度.
+	const D3DXVECTOR2	INIT_UV				= D3DXVECTOR2(0.0f, 0.0f);				//初期UV座標.
+	const D3DXVECTOR2	NORMAL_UV_POS		= D3DXVECTOR2(0.0f, 0.0f);				//通常UV座標.
+	const D3DXVECTOR2	ENTER_UV_POS		= D3DXVECTOR2(0.0f, 1.0f);				//決定時のUV座標.
 
 	//=====================関数=====================//.
 	void UpDate();		//更新処理関数.
