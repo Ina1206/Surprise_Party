d4325c6d0ac7b8e0899a6ea79459c0f1bef27b96
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-05-22 09:56:34 +0900

    煙エフェクトクラスの追加

---
+++ Surprise_Party/Surprise_Party/SourceCode/SpriteEffect/SmokeEffect/CSmokeEffect.h
@@ -0,0 +1,29 @@
+#ifndef CSMOKE_EFFECT_H
+#define CSMOKE_EFFECT_H
+
+#include "..\CSpriteEffectBase.h"
+
+/*************************************
+*		煙エフェクトクラス.
+**********/
+class CSmokeEffect
+	: public CSpriteEffectBase
+{
+public:
+	CSmokeEffect();
+	~CSmokeEffect();
+
+	//=====================関数=======================//.
+	void Update();							//更新処理関数.
+	void PlayStartInit(const int& num);		//エフェクト開始初期化処理関数.
+	
+private:
+	//=====================関数=======================//.
+	void Init();							//初期化処理関数.
+	void Release();							//解放処理関数.
+	void AppeartJudgement(const int& num);	//表示判定処理関数.
+	void Move(const int& num);				//移動処理関数.
+
+};
+
+#endif	//#ifndef CSMOKE_EFFECT_H.
