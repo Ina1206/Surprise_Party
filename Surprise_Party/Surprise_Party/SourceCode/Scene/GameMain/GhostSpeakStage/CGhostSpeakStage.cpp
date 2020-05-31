c988a86d5e9b36af8c2b8ee6c5a967d4902ea933
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-05-31 22:41:47 +0900

    ライト色の設定修正

--- Surprise_Party/Surprise_Party/SourceCode/Scene/GameMain/GhostSpeakStage/CGhostSpeakStage.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/GameMain/GhostSpeakStage/CGhostSpeakStage.cpp
@@ -131,8 +131,7 @@ void CGhostSpeakStage::Init()
 	m_stLight.fLightPosWidth = 20.0f;
 	m_stLight.m_fLightWidth = 10.0f;
 	m_stLight.m_fLightMax = 1.0f;
-	m_stLight.vLightColor = D3DXVECTOR3(1.0f, 201.0f /255.0f, 14.0f / 255.0f);
-	m_stLight.vLightColor = D3DXVECTOR3(243.0f / 255.0f, 232.0f / 255.0f, 12.0f / 255.0f);
+	m_stLight.vLightColor = D3DXVECTOR3(243.0f, 232.0f, 12.0f) / 255.0f;
 	m_stLight.vPos = D3DXVECTOR3(26.2f, 11.7f, 6.7f);
 	D3DXMatrixRotationYawPitchRoll(&m_stLight.mRot, 0.0f, 0.0f, 0.0f);
 	m_stLight.fIntensity = 27.0f;
