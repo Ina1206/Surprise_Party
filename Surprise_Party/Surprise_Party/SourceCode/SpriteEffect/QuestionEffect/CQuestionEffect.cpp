3fa3cc1e356761b4e2abb9a8d1f7638cef7aac9c
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-05-31 20:38:15 +0900

    QuestionMarkの移動処理追加

--- Surprise_Party/Surprise_Party/SourceCode/SpriteEffect/QuestionEffect/CQuestionEffect.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/SpriteEffect/QuestionEffect/CQuestionEffect.cpp
@@ -4,6 +4,9 @@
 *		疑問エフェクトクラス.
 ***************/
 CQuestionEffect::CQuestionEffect()
+	: m_fAcc			(0.0f)
+	, m_vStartPos		(0.0f, 0.0f, 0.0f)
+	, m_MoveFinishFlag	(0)
 {
 	//初期化処理.
 	Init();
@@ -20,7 +23,15 @@ CQuestionEffect::~CQuestionEffect()
 //========================================.
 void CQuestionEffect::Update()
 {
+	const int SPRITE_NUM = 0;
 
+	//例外処理.
+	if (m_MoveFinishFlag & FINISH_MOVE_VERTICAL) {
+		return;
+	}
+
+	//移動処理関数.
+	Move(SPRITE_NUM);
 }
 
 //========================================.
@@ -28,7 +39,16 @@ void CQuestionEffect::Update()
 //========================================.
 void CQuestionEffect::PlayStartInit(const int& num)
 {
+	//初期値設定処理関数.
+	SettingDefaultValue(num);
+	m_fAlpha[num] = 1.0f;
+	m_fScale[num] = 1.0f;
+
+	m_fAcc = 0.0f;
 	m_vPos[num] = m_vCenterPos;
+	m_vStartPos = m_vPos[num];
+	
+	m_MoveFinishFlag = 0;
 }
 
 //========================================.
@@ -66,5 +86,26 @@ void CQuestionEffect::AppeartJudgement(const int& num)
 //========================================.
 void CQuestionEffect::Move(const int& num)
 {
+	m_fAcc -= ACC_SPEAD;
+	if (m_vPos[num].y < m_vStartPos.y) {
+		m_fAcc = 0.0f;
+
+		if (m_MoveFinishFlag & FINISH_MOVE_HORIZONTAL) {
+			m_MoveFinishFlag |= FINISH_MOVE_VERTICAL;
+		}
+	}
+	m_vPos[num].y += m_fAcc + GRAVITY;
+
+	//横移動終了の場合したまで処理しない.
+	if (m_MoveFinishFlag & FINISH_MOVE_HORIZONTAL) {
+		return;
+	}
+
+	m_fDistance[num] += MOVE_SPEED;
+	if (m_fDistance[num] > DISTANCE_MAX) {
+		m_fDistance[num] = DISTANCE_MAX;
+		m_MoveFinishFlag |= FINISH_MOVE_HORIZONTAL;
+	}
+	m_vPos[num].x = m_vStartPos.x + m_fDistance[num];
 
 }
--- Surprise_Party/Surprise_Party/SourceCode/SpriteEffect/QuestionEffect/CQuestionEffect.h
+++ Surprise_Party/Surprise_Party/SourceCode/SpriteEffect/QuestionEffect/CQuestionEffect.h
@@ -13,6 +13,15 @@ public:
 	CQuestionEffect();
 	~CQuestionEffect();
 
+	//====================定数========================//.
+	const unsigned int	FINISH_MOVE_HORIZONTAL	= (1 << 0);	//横移動終了フラグ.
+	const unsigned int	FINISH_MOVE_VERTICAL	= (1 << 1);	//縦移動終了フラグ.
+
+	const float			ACC_SPEAD				= 0.004f;	//加速度の減少速度.
+	const float			GRAVITY					= 0.08f;	//重力.
+	const float			MOVE_SPEED				= 0.025f;	//移動速度.
+	const float			DISTANCE_MAX			= 1.5f;		//距離最大値.
+
 	//====================関数========================//.
 	void Update();							//更新処理関数.
 	void PlayStartInit(const int& num);		//エフェクト開始処理化処理関数.
@@ -23,6 +32,11 @@ private:
 	void Release();							//解放処理関数.
 	void AppeartJudgement(const int& num);	//表示判定処理関数.
 	void Move(const int& num);				//移動処理関数.
+
+	//====================変数========================//.
+	float			m_fAcc;					//加速度.
+	D3DXVECTOR3		m_vStartPos;			//移動開始時の座標.
+	unsigned int	m_MoveFinishFlag;		//移動フラグ.
 
 };
 
