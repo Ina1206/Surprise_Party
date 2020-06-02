bd60407c75d64ba2017f92822871a0cfbe9ac43d
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-06-02 12:20:36 +0900

    エンディング働くお化け基底クラス追加

---
+++ Surprise_Party/Surprise_Party/SourceCode/GameObject/Chara/Ghost/WorkGhost/EndingWorkGhost/CEndingWorkGhostBase.cpp
@@ -0,0 +1,14 @@
+#include "CEndingWorkGhostBase.h"
+
+/**************************************************
+*		エンディング働くお化け基底クラス.
+********************/
+CEndingWorkGhostBase::CEndingWorkGhostBase()
+{
+
+}
+
+CEndingWorkGhostBase::~CEndingWorkGhostBase()
+{
+
+}
---
+++ Surprise_Party/Surprise_Party/SourceCode/GameObject/Chara/Ghost/WorkGhost/EndingWorkGhost/CEndingWorkGhostBase.h
@@ -0,0 +1,23 @@
+#ifndef CENDING_WORK_GHOST_BASE_H
+#define CENDING_WORK_GHOST_BASE_H
+
+#include "..\CWorkGhostBase.h"
+
+/**************************************************
+*		エンディング働くお化け基底クラス.
+********************/
+class CEndingWorkGhostBase
+	: public CWorkGhostBase
+{
+public:
+	CEndingWorkGhostBase();
+	virtual ~CEndingWorkGhostBase();
+
+	
+protected:
+
+private:
+
+};
+
+#endif	//#ifndef CENDING_WORK_GHOST_BASE_H.
