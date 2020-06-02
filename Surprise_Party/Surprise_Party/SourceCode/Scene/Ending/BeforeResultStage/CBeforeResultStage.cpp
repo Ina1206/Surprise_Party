dcd4939ffe285a70a02d75b3280c699014aa5840
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-06-02 11:07:24 +0900

    カメラ座標設定

--- Surprise_Party/Surprise_Party/SourceCode/Scene/Ending/BeforeResultStage/CBeforeResultStage.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/Ending/BeforeResultStage/CBeforeResultStage.cpp
@@ -41,6 +41,8 @@ void CBeforeResultStage::Render()
 //=======================================.
 void CBeforeResultStage::Init()
 {
+	m_Camera.vPos = D3DXVECTOR3(5.0f, 2.5f, -3.5f);
+	m_Camera.vLook = D3DXVECTOR3(5.0f, 2.5f, 5.0f);
 
 }
 
