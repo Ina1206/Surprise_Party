5ea61f1c1836b6227c3f7d67f26a505f6ce51562
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-06-02 10:54:45 +0900

    描画初期化処理関数の追加

--- Surprise_Party/Surprise_Party/SourceCode/Scene/Ending/CEndingStageBase/CEndingStageBase.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/Ending/CEndingStageBase/CEndingStageBase.cpp
@@ -4,7 +4,10 @@
 *		エンディングステージ基底クラス.
 *****************/
 CEndingStageBase::CEndingStageBase()
-	: m_pCBigGhost			(nullptr)
+	: m_mView				()
+	, m_mProj				()
+	, m_Camera				()
+	, m_pCBigGhost			(nullptr)
 	, m_pCFloor				(nullptr)
 	, m_pCBackstageLight	(nullptr)
 {
@@ -18,10 +21,22 @@ CEndingStageBase::~CEndingStageBase()
 }
 
 //=========================================.
+//		描画初期化処理関数.
+//=========================================.
+void CEndingStageBase::RenderInitSetting(const D3DXMATRIX& mView, const D3DXMATRIX& mProj)
+{
+	m_mView = mView;
+	m_mProj = mProj;
+}
+
+//=========================================.
 //		床の描画処理関数.
 //=========================================.
 void CEndingStageBase::RenderFloor()
 {
+	//ライト情報.
+	const LIGHT m_Light = m_pCBackstageLight->GetLight();
+
 	m_pCFloor->SetScale(0.5f);
 	m_pCFloor->SetCameraPos(m_Camera.vPos);
 	m_pCFloor->RenderInitSetting(m_mView, m_mProj, m_Light);
--- Surprise_Party/Surprise_Party/SourceCode/Scene/Ending/CEndingStageBase/CEndingStageBase.h
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/Ending/CEndingStageBase/CEndingStageBase.h
@@ -15,24 +15,32 @@ public:
 	virtual ~CEndingStageBase();
 
 	//========================関数=============================//.
-	virtual void Update() = 0;	//更新処理関数.
-	virtual void Render() = 0;	//描画処理関数.
+	virtual void Update() = 0;								//更新処理関数.
+	virtual void Render() = 0;								//描画処理関数.
+	
+	void RenderInitSetting( const D3DXMATRIX& mView, 
+							const D3DXMATRIX& mProj);		//描画初期設定処理関数.
 
 protected:
 	//========================関数=============================//.
-	virtual void Init() = 0;	//初期化処理関数.
-	virtual void Release() = 0;	//解放処理関数.
+	virtual void Init() = 0;								//初期化処理関数.
+	virtual void Release() = 0;								//解放処理関数.
 
-	void RenderFloor();			//床の描画処理関数.
+	void RenderFloor();										//床の描画処理関数.
 
 	//========================変数=============================//.
+	D3DXMATRIX							m_mView;			//ビュー行列.
+	D3DXMATRIX							m_mProj;			//プロジェクション行列.
+	CAMERA								m_Camera;			//カメラ構造体.
 	std::unique_ptr<CBigGhost>			m_pCBigGhost;		//ビッグゴーストクラス.
 	std::unique_ptr<CFloor>				m_pCFloor;			//床クラス.
 	std::unique_ptr<CBackstageLight>	m_pCBackstageLight;	//舞台裏ライトクラス.
 
+
+
 private:
 	//========================関数=============================//.
-	void InitCommonValue();		//共通値の初期化処理関数.
+	void InitCommonValue();									//共通値の初期化処理関数.
 
 
 };
