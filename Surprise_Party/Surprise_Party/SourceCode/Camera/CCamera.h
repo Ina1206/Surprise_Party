1bea6ce5d81ceffb8a089b3663f4228e28ea582d
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-06-04 21:22:29 +0900

    カメラクラス追加

---
+++ Surprise_Party/Surprise_Party/SourceCode/Camera/CCamera.h
@@ -0,0 +1,28 @@
+#ifndef CCAMERA_BASE_H
+#define CCAMERA_BASE_H
+
+#include "..\Drawing\Resource\CResourceManager.h"
+
+/**************************************
+*		カメラ基底クラス.
+**************/
+class CCamera
+{
+public:
+	CCamera();
+	~CCamera();
+
+	//=======================関数==========================//.
+	void Update();		//更新処理関数.
+
+private:
+	//=======================関数==========================//.
+	void Init();		//初期化処理関数.
+	void Release();		//解放処理関数.
+
+	//=======================変数==========================//.
+	CAMERA	m_Camera;	//カメラ情報.
+
+};
+
+#endif	//#ifndef CCAMERA_BASE_H.
--- Surprise_Party/Surprise_Party/SourceCode/Scene/Ending/BeforeResultStage/CBeforeResultStage.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/Ending/BeforeResultStage/CBeforeResultStage.cpp
@@ -99,7 +99,7 @@ void CBeforeResultStage::Render()
 //=======================================.
 void CBeforeResultStage::Init()
 {
-	m_Camera.vPos = D3DXVECTOR3(5.0f, 2.5f, -3.5f);
+	m_Camera.vPos = D3DXVECTOR3(23.9f, 5.0f, -3.5f);
 	m_Camera.vLook = D3DXVECTOR3(5.0f, 2.5f, 5.0f);
 
 }
