9c0961618c8b6f341b463e55a179e320203b6648
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-06-05 13:58:41 +0900

    白スクリーンフェードクラスを追加

---
+++ Surprise_Party/Surprise_Party/SourceCode/UI/Fade/WhiteScreenFade/CWhiteScreenFade.h
@@ -0,0 +1,28 @@
+#ifndef CWHITE_SCREEN_FADE_H
+#define CWHITE_SCREEN_FADE_H
+
+#include "..\CFade.h"
+
+/******************************************
+*		白スクリーンフェードクラス.
+************/
+class CWhiteScreenFade
+	: public CFade
+{
+public:
+	CWhiteScreenFade();
+	~CWhiteScreenFade();
+
+	//========================関数=========================//.
+	void Update();		//更新処理関数.
+	void Render();		//描画処理関数.
+
+private:
+	//========================関数=========================//.
+	void Init();		//初期化処理関数.
+	void Release();		//解放処理関数.
+
+
+};
+
+#endif	//#ifndef CWHITE_SCREEN_FADE_H.
