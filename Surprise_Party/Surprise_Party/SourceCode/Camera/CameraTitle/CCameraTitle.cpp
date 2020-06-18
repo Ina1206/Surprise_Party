651cdf5d21dd31177db4b844496b551c37ba6cdc
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-06-18 12:31:49 +0900

    カメラの移動処理追加

--- Surprise_Party/Surprise_Party/SourceCode/Camera/CameraTitle/CCameraTitle.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/Camera/CameraTitle/CCameraTitle.cpp
@@ -4,6 +4,9 @@
 *		タイトルカメラクラス.
 *************/
 CCameraTitle::CCameraTitle()
+	: m_MoveTypeFlag	(0)
+	, m_fMoveDistance	(0.0f)
+	, m_fMoveSpeed		(0.0f)
 {
 	//初期化処理関数.
 	Init();
@@ -20,52 +23,75 @@ CCameraTitle::~CCameraTitle()
 //==========================================.
 void CCameraTitle::Update()
 {
-	const unsigned int PosFlag = (1 << 0);
-	const unsigned int LookFlag = (1 << 1);
-	static int Cnt = 0;
-	static D3DXVECTOR3 vLookPos = D3DXVECTOR3(5.0f, -0.8f, 5.0f);
-	if (GetAsyncKeyState('1') & 0x0001) {
-		Cnt++;
-		if (Cnt > 1) {
-			Cnt = 0;
+	//const unsigned int PosFlag = (1 << 0);
+	//const unsigned int LookFlag = (1 << 1);
+	//const unsigned int MoveFlag = (1 << 2);
+	//static int Cnt = 0;
+	//static D3DXVECTOR3 vLookPos = D3DXVECTOR3(5.0f, -0.8f, 5.0f);
+	//if (GetAsyncKeyState('1') & 0x0001) {
+	//	Cnt++;
+	//	if (Cnt > 2) {
+	//		Cnt = 0;
+	//	}
+	//}
+
+	//D3DXVECTOR3 Tmp;
+	//if (1 << Cnt & PosFlag) {
+	//	Tmp = m_Camera.vPos;
+	//}
+	//else if (1 << Cnt & LookFlag) {
+	//	Tmp = m_Camera.vLook;
+	//}
+
+	//if (GetAsyncKeyState(VK_UP) & 0x8000) {
+	//	Tmp.y += 0.1f;
+	//}
+	//if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+	//	Tmp.y -= 0.1f;
+	//}
+	//if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+	//	Tmp.x += 0.1f;
+	//}
+	//if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
+	//	Tmp.x -= 0.1f;
+	//}
+	//if (GetAsyncKeyState('Z') & 0x8000) {
+	//	Tmp.z += 0.1f;
+	//}
+	//if (GetAsyncKeyState('X') & 0x8000) {
+	//	Tmp.z -= 0.1f;
+	//}
+
+	//if (1 << Cnt & PosFlag) {
+	//	m_Camera.vPos = Tmp;
+	//}
+	//else if (1 << Cnt & LookFlag) {
+	//	m_Camera.vLook = Tmp;
+	//}
+	//else if (1 << Cnt & MoveFlag) {
+	//	m_Camera.vLook.x += MOVE_SPEED_FROM_OBLIQUE;
+	//	m_Camera.vPos.x += MOVE_SPEED_FROM_OBLIQUE;
+	//}
+
+	
+	m_fMoveDistance += m_fMoveSpeed;
+	m_Camera.vPos.x += m_fMoveSpeed;
+	m_Camera.vLook.x += m_fMoveSpeed;
+
+	if (m_fMoveDistance >= 50.0f) {
+		m_fMoveDistance = 0.0f;
+
+		if (m_fMoveSpeed >= MOVE_SPEED_FROM_FRONT) {
+			m_Camera.vPos = POS_FROM_OBLIQUE;
+			m_Camera.vLook = LOOK_FROM_OBLIQUE;
+			m_fMoveSpeed = MOVE_SPEED_FROM_OBLIQUE;
+			return;
 		}
-	}
 
-	D3DXVECTOR3 Tmp;
-	if (1 << Cnt & PosFlag) {
-		Tmp = m_Camera.vPos;
+		m_Camera.vPos = POS_FROM_FRONT;
+		m_Camera.vLook = LOOK_FROM_FRONT;
+		m_fMoveSpeed = MOVE_SPEED_FROM_FRONT;
 	}
-	else if (1 << Cnt & LookFlag) {
-		Tmp = m_Camera.vLook;
-	}
-
-	if (GetAsyncKeyState(VK_UP) & 0x8000) {
-		Tmp.y += 0.1f;
-	}
-	if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
-		Tmp.y -= 0.1f;
-	}
-	if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
-		Tmp.x += 0.1f;
-	}
-	if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
-		Tmp.x -= 0.1f;
-	}
-	if (GetAsyncKeyState('Z') & 0x8000) {
-		Tmp.z += 0.1f;
-	}
-	if (GetAsyncKeyState('X') & 0x8000) {
-		Tmp.z -= 0.1f;
-	}
-
-	if (1 << Cnt & PosFlag) {
-		m_Camera.vPos = Tmp;
-	}
-	else if (1 << Cnt & LookFlag) {
-		m_Camera.vLook = Tmp;
-	}
-
-
 }
 
 //==========================================.
@@ -73,8 +99,10 @@ void CCameraTitle::Update()
 //==========================================.
 void CCameraTitle::Init()
 {
-	m_Camera.vPos = D3DXVECTOR3(11.9f, 7.5f, -19.9f);
-	m_Camera.vLook = D3DXVECTOR3(11.9f, 5.3f, 4.0f);
+	//m_Camera.vPos = D3DXVECTOR3(11.9f, 7.5f, -19.9f);
+	m_Camera.vPos = POS_FROM_FRONT;
+	m_Camera.vLook = LOOK_FROM_FRONT;
+	m_fMoveSpeed = MOVE_SPEED_FROM_FRONT;
 }
 
 //==========================================.
--- Surprise_Party/Surprise_Party/SourceCode/Camera/CameraTitle/CCameraTitle.h
+++ Surprise_Party/Surprise_Party/SourceCode/Camera/CameraTitle/CCameraTitle.h
@@ -13,6 +13,21 @@ public:
 	CCameraTitle();
 	~CCameraTitle();
 
+	//=======================定数==========================//.
+	const unsigned int	MOVE_FROM_FRONT_FLAG	= (1 << 0);							//正面移動フラグ.
+	const unsigned int	MOVE_FROM_OBLIQUE_FLAG	= (1 << 1);							//斜めカメラ移動フラグ.
+	
+	const D3DXVECTOR3	POS_FROM_FRONT			= D3DXVECTOR3(11.9f, 3.3f, -14.3f);	//正面から見るカメラ座標.
+	const D3DXVECTOR3	LOOK_FROM_FRONT			= D3DXVECTOR3(11.9f, 3.3f, 4.0f);	//正面から見るカメラの注視点座標.
+	
+	const D3DXVECTOR3	POS_FROM_OBLIQUE		= D3DXVECTOR3(1.3f, 5.4f, -5.5f);	//斜めから見るカメラ座標.
+	const D3DXVECTOR3	LOOK_FROM_OBLIQUE		= D3DXVECTOR3(6.4f, 4.6f, 4.0f);	//斜めから見るカメラの注視点.
+
+	const float			MOVE_SPEED_FROM_FRONT	= 0.05f;							//正面から見るカメラ移動速度.
+	const float			MOVE_SPEED_FROM_OBLIQUE = 0.02f;							//斜めから見るカメラ移動速度.
+
+	const int			MOVE_TYPE_MAX			= 2;								//移動タイプ最大値.
+
 	//=======================関数==========================//.
 	void Update();		//更新処理関数.
 	
@@ -21,7 +36,10 @@ private:
 	void Init();		//初期化処理関数.
 	void Release();		//解放処理関数.
 
-
+	//=======================変数==========================//.
+	unsigned int	m_MoveTypeFlag;		//移動種類フラグ.
+	float			m_fMoveDistance;	//移動距離.
+	float			m_fMoveSpeed;		//移動速度.
 };
 
 #endif	//#ifndef CCAMERA_TITLE_H.
