f523faa7c24e9d32b446b059e19530e790583135
紀子 稲森 <no.yozakura.ri@gmail.com>
2020-07-05 21:10:46 +0900

    コメントのインデントを修正しました。

--- Surprise_Party/Surprise_Party/SourceCode/UI/PlayUI/ClosedTime/CClosedTime.h
+++ Surprise_Party/Surprise_Party/SourceCode/UI/PlayUI/ClosedTime/CClosedTime.h
@@ -15,18 +15,18 @@ public:
 	~CClosedTime();
 
 	//====================定数=====================//.
-	const int			TIME_UI_MAX				= 7;	//時間UI最大数.
-	const int			TIME_START_NUM			= 2;	//時間のスプライト開始番号.
-	const int			TREE_BORD				= 0;	//木の板番号.
-	const int			CLOSED_STRING_NUM		= 1;	//閉店文字.
-	const int			HOUR_DELIMITER			= 60;	//時間(h)区切り.
-	const int			NUM_RANK_DELIMITER		= 10;	//数値区切り.
-	const int			TIME_REDUCE_DELIMITER	= 60;	//時間減らす区切り.
-	const int			TIME_MIN				= 0;	//時間最小値.
-	const int			BONUS_TIME_ADD			= 30;	//ボーナス追加時間.
+	const int			TIME_UI_MAX				= 7;									//時間UI最大数.
+	const int			TIME_START_NUM			= 2;									//時間のスプライト開始番号.
+	const int			TREE_BORD				= 0;									//木の板番号.
+	const int			CLOSED_STRING_NUM		= 1;									//閉店文字.
+	const int			HOUR_DELIMITER			= 60;									//時間(h)区切り.
+	const int			NUM_RANK_DELIMITER		= 10;									//数値区切り.
+	const int			TIME_REDUCE_DELIMITER	= 60;									//時間減らす区切り.
+	const int			TIME_MIN				= 0;									//時間最小値.
+	const int			BONUS_TIME_ADD			= 30;									//ボーナス追加時間.
 
-	const D3DXVECTOR2	TREE_PATTARN_NO			= D3DXVECTOR2(1.0f, 0.0f);	//木の板のパターン番号.
-	const D3DXVECTOR2	COLON_PATTARN_NO		= D3DXVECTOR2(10.0f, 0.0f);	//コロンパターン番号.
+	const D3DXVECTOR2	TREE_PATTARN_NO			= D3DXVECTOR2(1.0f, 0.0f);				//木の板のパターン番号.
+	const D3DXVECTOR2	COLON_PATTARN_NO		= D3DXVECTOR2(10.0f, 0.0f);				//コロンパターン番号.
 	
 	const D3DXVECTOR3	COLON_POS				= D3DXVECTOR3(1110.0f, 640.0f, 0.0f);	//木の板の座標.
 	const D3DXVECTOR3	TREE_BORD_POS			= D3DXVECTOR3(980.0f, 580.0f, 0.0f);	//コロンの座標.
