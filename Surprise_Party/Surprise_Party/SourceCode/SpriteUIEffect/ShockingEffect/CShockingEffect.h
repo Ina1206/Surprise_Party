2249fe7c489141681c53dc0de8f997eebbf23295
hera <hera@R201-32>
2020-07-06 11:51:23 +0900

    衝撃エフェクトクラスを追加しました。

---
+++ Surprise_Party/Surprise_Party/SourceCode/SpriteUIEffect/ShockingEffect/CShockingEffect.h
@@ -0,0 +1,24 @@
+#ifndef CSHOCKING_EFFECT_H
+#define CSHOCKING_EFFECT_H
+
+#include "..\CSpriteUIEffectBase.h"
+
+/*************************************
+*		衝撃エフェクトクラス.
+****************/
+class CShockingEffect
+	: public CSpriteUIEffectBase
+{
+public:
+	CShockingEffect();
+	~CShockingEffect();
+
+	//======================関数============================//.
+	void Update();	//更新処理関数.
+	void Render();	//描画処理関数.
+
+private:
+
+};
+
+#endif	//#ifndef CSHOCKING_EFFECT_H.
