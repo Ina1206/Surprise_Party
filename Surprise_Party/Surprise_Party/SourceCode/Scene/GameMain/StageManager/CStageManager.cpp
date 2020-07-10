a1f3f708279e12ed61ee923f546865f0e519db46
hera <hera@R201-32>
2020-07-10 19:25:10 +0900

    ステージ調整

--- Surprise_Party/Surprise_Party/SourceCode/Scene/GameMain/StageManager/CStageManager.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/Scene/GameMain/StageManager/CStageManager.cpp
@@ -150,7 +150,7 @@ void CStageManager::ChangeStage()
 	switch (static_cast<CStageBase::enStageType>(m_StageType)) {
 	case CStageBase::enStageType::GhostSpeakStage:
 		//次のステージへ.
-		m_AllEndingType = 2;
+		m_AllEndingType = 3;
 		m_pCStageBase[STAGE_TYPE_NUM].reset(new CMainStage(m_AllEndingType, CStageBase::enStageType::GhostSpeakStage , m_enBeforeEndingType));
 		m_bOldTutorialFlag = 0;
 		break;
