234038b6c91dcd9ebac82f679f7a55dfabc50bb7
hera <hera@R201-32>
2020-07-10 12:28:33 +0900

    コメントを追加しました。

--- Surprise_Party/Surprise_Party/SourceCode/UI/SceneUI/TitleUI/TitleStringUI/CTitleStringUI.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/UI/SceneUI/TitleUI/TitleStringUI/CTitleStringUI.cpp
@@ -55,8 +55,11 @@ void CTitleStringUI::Update()
 void CTitleStringUI::Render()
 {
 	for (int title = 0; title < TITLE_STRING_MAX; title++) {
+		//座標.
 		m_pCSpriteUI[title]->SetPosition(m_vTitlePos[title]);
+		//UV.
 		m_pCSpriteUI[title]->SetPattern(m_vTitleUV[title]);
+		//描画.
 		m_pCDepthStencil->SetDepth(false);
 		m_pCSpriteUI[title]->Render();
 		m_pCDepthStencil->SetDepth(true);
@@ -68,13 +71,16 @@ void CTitleStringUI::Render()
 //==========================================.
 void CTitleStringUI::Init()
 {
+	//スプライトUI情報.
 	SPRITE_STATE SpriteState = m_pCResourceManager->GetSpriteUIState(enSpriteUI::Title);
 
+	//値を初期化処理.
 	for (int title = 0; title < TITLE_STRING_MAX; title++) {
 		m_pCSpriteUI.push_back(m_pCResourceManager->GetSpriteUI(enSpriteUI::Title));
 		m_vTitleUV.push_back(D3DXVECTOR2(0.0f, 1.0f * title));
 		m_vTitlePos.push_back(TITLE_POS);
 
+		//画面外に配置する処理.
 		m_vTitlePos[title].x = WND_W;
 		if (title == 0) {
 			m_vTitlePos[title].x = -SpriteState.Disp.w;
@@ -114,6 +120,7 @@ void CTitleStringUI::FetchPos()
 {
 	m_vFetchGhostPos = m_vTitlePos[m_FetchStringNum];
 
+	//一番最後の文字は３分の２のところに配置.
 	if (m_FetchStringNum == m_vTitlePos.size() - 1) {
 		SPRITE_STATE SpriteState = m_pCResourceManager->GetSpriteUIState(enSpriteUI::Title);
 		m_vFetchGhostPos.x += (SpriteState.Disp.w / 3.0f) * 2.0f;
@@ -127,6 +134,7 @@ void CTitleStringUI::Move()
 {
 	m_vTitlePos[m_FetchStringNum].x = m_vBeforeMovePos[m_FetchStringNum].x + m_fMoveDistance;
 
+	//移動終了.
 	if (fabsf(m_vTitlePos[m_FetchStringNum].x - m_vLastPos[m_FetchStringNum].x) < m_fErrorRange) {
 		m_vTitlePos[m_FetchStringNum] = m_vLastPos[m_FetchStringNum];
 		m_bStopFlag[m_FetchStringNum] = true;
