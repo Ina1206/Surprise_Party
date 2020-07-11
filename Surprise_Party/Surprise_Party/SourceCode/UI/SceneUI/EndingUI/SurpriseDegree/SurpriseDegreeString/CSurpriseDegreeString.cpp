7d5aa68555aa8d57d662a020513f652a046ae531
Kinamon <47658031+Kinamon@users.noreply.github.com>
2020-07-12 00:31:56 +0900

    デバッグ用の座標設定処理を追加しました。

--- Surprise_Party/Surprise_Party/SourceCode/UI/SceneUI/EndingUI/SurpriseDegree/SurpriseDegreeString/CSurpriseDegreeString.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/UI/SceneUI/EndingUI/SurpriseDegree/SurpriseDegreeString/CSurpriseDegreeString.cpp
@@ -20,7 +20,19 @@ CSurpriseDegreeString::~CSurpriseDegreeString()
 //==================================================.
 void CSurpriseDegreeString::Update()
 {
-
+	static D3DXVECTOR3 Pos(0.0f, 0.0f, 0.0f);
+	if (GetAsyncKeyState(VK_UP) & 0x8000) {
+		Pos.y -= 0.5f;
+	}
+	if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+		Pos.y += 0.5f;
+	}
+	if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
+		Pos.x -= 0.5f;
+	}
+	if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+		Pos.x += 0.5f;
+	}
 }
 
 //==================================================.
@@ -29,6 +41,7 @@ void CSurpriseDegreeString::Update()
 void CSurpriseDegreeString::Init()
 {
 	m_pCSpriteUI = m_pCResourceManager->GetSpriteUI(enSpriteUI::SurpriseDegree);
+	m_vEatchSizeScale = EATCH_SCALE_MAX;
 }
 
 //==================================================.
