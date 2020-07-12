e208d7d0848654ee32fce0edfcc1f7a274d599e9
Kinamon <47658031+Kinamon@users.noreply.github.com>
2020-07-12 23:24:57 +0900

    移動処理を追加しました。

--- Surprise_Party/Surprise_Party/SourceCode/UI/SceneUI/EndingUI/EvaluationUI/HightEvaluationString/CHightEvaluationString.cpp
+++ Surprise_Party/Surprise_Party/SourceCode/UI/SceneUI/EndingUI/EvaluationUI/HightEvaluationString/CHightEvaluationString.cpp
@@ -20,6 +20,9 @@ CHightEvaluationString::~CHightEvaluationString()
 //=========================================.
 void CHightEvaluationString::Update()
 {
+	if (m_bFinishedAllDispFlag == true) {
+		return;
+	}
 	if (GetAsyncKeyState(VK_UP) & 0x8000) {
 		for (unsigned int string = 0; string < m_pCEvaluationUI.size(); string++) {
 			m_vEvaluationPos[string].y += 0.5f;
@@ -31,6 +34,8 @@ void CHightEvaluationString::Update()
 			m_vEvaluationPos[string].y -= 0.5f;
 		}
 	}
+
+	Move();
 }
 
 //=========================================.
@@ -45,12 +50,54 @@ void CHightEvaluationString::Init()
 
 	//要素数の初期化処理関数.
 	InitElementCount();
+
+	for (unsigned int sprite = 0; sprite < m_pCEvaluationUI.size(); sprite++) {
+		m_vEvaluationPos[sprite] = INIT_POS;
+		m_vEvaluationUV[sprite] = D3DXVECTOR2(1.0f * sprite, 0.0f);
+	}
 }
 
 //=========================================.
 //		解放処理関数.
 //=========================================.
 void CHightEvaluationString::Release()
+{
+
+}
+
+//=========================================.
+//		移動処理関数.
+//=========================================.
+void CHightEvaluationString::Move()
+{
+	const SPRITE_STATE SpriteState = m_pCResourceManager->GetSpriteUIState(enSpriteUI::HightEvaluationString);
+	const float	FINISH_POS = FINISH_BASE_POS.x + (SpriteState.Disp.w * m_UpdateNum);
+	const float RAITO = FINISH_POS / FINISH_BASE_POS.x;
+
+	m_vEvaluationPos[m_UpdateNum].x += 0.3f * RAITO;
+
+	if (m_vEvaluationPos[m_UpdateNum].x >= FINISH_POS) {
+		m_vEvaluationPos[m_UpdateNum] = FINISH_BASE_POS;
+		m_vEvaluationPos[m_UpdateNum].x += SpriteState.Disp.w * m_UpdateNum;
+		m_UpdateNum++;
+		if (m_UpdateNum >= static_cast<int>(m_pCEvaluationUI.size())) {
+			m_bFinishedAllDispFlag = true;
+		}
+	}
+}
+
+//=========================================.
+//		ジャンプ処理関数.
+//=========================================.
+void CHightEvaluationString::Jump()
+{
+
+}
+
+//=========================================.
+//		バウンド処理関数.
+//=========================================.
+void CHightEvaluationString::Bound()
 {
 
 }
--- Surprise_Party/Surprise_Party/SourceCode/UI/SceneUI/EndingUI/EvaluationUI/HightEvaluationString/CHightEvaluationString.h
+++ Surprise_Party/Surprise_Party/SourceCode/UI/SceneUI/EndingUI/EvaluationUI/HightEvaluationString/CHightEvaluationString.h
@@ -13,6 +13,10 @@ public:
 	CHightEvaluationString();
 	~CHightEvaluationString();
 
+	//=====================定数======================//.
+	const D3DXVECTOR3	FINISH_BASE_POS	= D3DXVECTOR3(10.0f, 50.0f, 0.0f);	//最終基準位置.
+	const D3DXVECTOR3	INIT_POS		= D3DXVECTOR3(0.0f, 0.0f, 0.0f);	//初期位置.
+
 	//=====================関数======================//.
 	void Update();		//更新処理関数.
 
@@ -20,7 +24,14 @@ protected:
 	//=====================関数======================//.
 	void Init();		//初期化処理関数.
 	void Release();		//解放処理関数.
+	void Move();		//移動処理関数.
+	void Jump();		//ジャンプ処理関数.
+	void Bound();		//バウンド処理関数.
 
+	//=====================変数======================//.
+	float	m_fAcc;			//加速度.
+	int		m_JumpCnt;		//ジャンプカウント.
+	int		m_BoundCnt;		//バウンドカウント.
 };
 
 #endif	//#ifndef CHIGHT_EVALUATION_STRING_H.
